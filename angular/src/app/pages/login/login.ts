d0a8307f1f549c5bf8dfb762b3fa4c27575ff58b
André Rodrigues <oandredev@gmail.com>
2026-05-03 17:49:41 -0300

    feat: implement login screen with form validation and styling

--- angular/src/app/pages/login/login.ts
+++ angular/src/app/pages/login/login.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.minLength(3), Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    rememberMe: new FormControl(false),
+  });
+
+  loginError: boolean = false;
+
+  ngOnInit() {
+    // Qualquer mudança no formulário irá resetar o estado de erro
+    this.loginForm.valueChanges.subscribe(() => {
+      if (this.loginError) {
+        this.loginError = false;
+      }
+    });
+  }
+
+  onSubmit() {
+    this.loginError = true;
+
+    if (this.loginForm.valid) {
+      console.log('Request feito:', this.loginForm.value);
+    }
+  }
+}

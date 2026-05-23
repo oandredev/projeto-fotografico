14af43667eb8411dd9d453cb734b881aac407982
André Rodrigues <oandredev@gmail.com>
2026-05-22 22:18:54 -0300

    feat: adds integration for dynamic content in the about section

--- angular/src/app/sections/public/about/about.ts
+++ angular/src/app/sections/public/about/about.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { AboutService } from '../../../services/about/about';
+import { environment } from '../../../environment';
 
 @Component({
   selector: 'app-about',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
-  
-export class About {}
+export class About implements OnInit {
+  constructor(private aboutService: AboutService) {}
+
+  aboutText = signal('');
+  aboutImage = signal<string | null>(null);
+
+  ngOnInit(): void {
+    this.loadAbout();
+  }
+
+  loadAbout(): void {
+    this.aboutService.find().subscribe({
+      next: (about) => {
+        if (!about) return;
+
+        this.aboutText.set(about.presentationText);
+
+        if (about.imageUrl) {
+          const url = `http://localhost:${environment.API_PORT}${about.imageUrl}`;
+
+          this.aboutImage.set(url);
+        } else {
+          this.aboutImage.set(null);
+        }
+      },
+      error: (error) => console.error(error),
+    });
+  }
+}

f16f9a84a583fbbfb1f9bff846b06917705c86ea
André Rodrigues <oandredev@gmail.com>
2026-05-23 22:05:25 -0300

    feat: adds integration for dynamic content in the hero section

--- angular/src/app/sections/public/hero/hero.ts
+++ angular/src/app/sections/public/hero/hero.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroService } from '../../../services/hero/hero';
+import { environment } from '../../../environment';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
-  
-export class Hero {}
+export class Hero implements OnInit {
+  constructor(private heroService: HeroService) {}
+
+  slogan = signal('');
+  imageUrls = signal<string[]>([]);
+
+  readonly API_URL = `http://localhost:${environment.API_PORT}`;
+
+  ngOnInit(): void {
+    this.heroService.find().subscribe({
+      next: (hero) => {
+        if (!hero) return;
+        this.slogan.set(hero.slogan ?? '');
+        this.imageUrls.set(hero.imageUrls ?? []);
+      },
+      error: (err) => console.error(err),
+    });
+  }
+}

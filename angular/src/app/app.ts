f267dbd61d1a2d415aa2562c3b72d28bce3a6a02
André Rodrigues <oandredev@gmail.com>
2026-06-10 20:53:39 -0300

    fix: adjustment to the JWT key and API port in Angular

--- angular/src/app/app.ts
+++ angular/src/app/app.ts
@@ -1,15 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
 import { Footer } from './shared/footer/footer';
+import { environment } from './environment';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header,Footer],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-  
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('meu-app');
+
+  ngOnInit(): void {
+    console.log(`[BOOT] API rodando na porta: ${environment.API_PORT}`);
+  }
 }
